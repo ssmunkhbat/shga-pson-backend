b576b964dc7cd7fb693eae484e34978bc466ccea
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-12-17 10:50:10 +0800

    added table
    .

--- main/src/modules/user/user.controller.ts
+++ main/src/modules/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt.guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+    
+    @UseGuards(JwtAuthGuard)
+    @Get('list')
+    async getUsers(@Query('limit') limit, @Query('page') page, @Query('search') search) {
+        return await this.userService.getUsers({ limit, page }, search);
+    }
+}
--- main/src/modules/user/user.service.ts
+++ main/src/modules/user/user.service.ts
@@ -1,37 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IPaginationOptions, paginate } from 'nestjs-typeorm-paginate';
 import { UserKey } from 'src/entity/userKey.entity';
 import { Repository } from 'typeorm';
 const md5 = require('md5');
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserKey)
+    private usersRepository: Repository<UserKey>,
+  ) { }
 
-    constructor(
-      @InjectRepository(UserKey)
-      private usersRepository: Repository<UserKey>,
-    ) { }
+  findByUsername(username: string) {
+    return this.usersRepository.findOne({ where: { userName: username } });
+  }
 
-    
-    findByUsername(username: string) {
-      return this.usersRepository.findOne({ where: { userName: username } });
+  findById(id: number) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async validateUser(username: string, password: string) {
+    const user = await this.findByUsername(username);
+    console.log('-----------validateUser-----------', user)
+    if (!user) return null;
+
+    const passwordHash = md5(password)
+    if (passwordHash !== user.password) {
+      return null;
     }
 
-    findById(id: number) {
-      return this.usersRepository.findOne({ where: { id } });
-    }
+    return user;
+  }
 
-    async validateUser(username: string, password: string) {
-      const user = await this.findByUsername(username);
-      console.log('-----------validateUser-----------', user)
-      if (!user) return null;
-
-      const passwordHash = md5(password)
-      if (passwordHash !== user.password) {
-        return null;
-      }
-
-      return user;
-    }
-
+  async getUsers(options: IPaginationOptions, searchParam) {
+    const queryBuilder = this.usersRepository.createQueryBuilder('uk')
+      .orderBy('uk.createdDate', 'DESC')
+    const data = await paginate<UserKey>(queryBuilder, options);
+    return { rows: data.items, total: data.meta.totalItems }
+  }
 }
