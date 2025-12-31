0ba73df404162a3c42e1cf1131a376a100df54c1
ssmunkhbat <47767779+ssmunkhbat@users.noreply.github.com>
2025-12-31 14:22:58 +0800

    updated
    .

--- main/src/controllers.ts
+++ main/src/controllers.ts
@@ -1,7 +1,9 @@
+import { RefsController } from "./modules/refs/refs.controller";
 import { SettingsController } from "./modules/settings/settings.controller";
 import { TableConfigController } from "./modules/table-config/table-config.controller";
 
 export const controllers = [
     TableConfigController,
     SettingsController,
+    RefsController,
 ]
