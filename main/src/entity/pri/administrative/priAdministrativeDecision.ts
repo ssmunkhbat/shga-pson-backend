f7539f144518c86eb77782ec0cca8fe1d21dd087
muujig-qwer <enkhtuul@smartsolution.mn>
2026-02-28 18:10:28 +0800

    --fix employee/decision
    1

--- main/src/entity/pri/administrative/priAdministrativeDecision.ts
+++ main/src/entity/pri/administrative/priAdministrativeDecision.ts
@@ -1,69 +1,119 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
 import { PriAdministrativeDecType } from './priAdministrativeDecType';
 import { PriEmployee } from '../employee/priEmployee';
 import { PriInfoDepartment } from 'src/entity/info/priInfoDepartment';
 
 @Entity('PRI_ADMINISTRATIVE_DECISION')
-export class PriAdministrativeDecision{
-
-  @PrimaryGeneratedColumn({ name: "ADMINISTRATIVE_DECISION_ID" })
+export class PriAdministrativeDecision {
+  @PrimaryGeneratedColumn({ name: 'ADMINISTRATIVE_DECISION_ID' })
   administrativeDecisionId: number;
 
-  @Column({ name: "ADMINISTRATIVE_DECISION_NUMBER" })
+  @Column({ name: 'ADMINISTRATIVE_DECISION_NUMBER' })
   administrativeDecisionNumber: string;
 
-  @Column({ name: "DECISION_DATE" })
+  @Column({ name: 'DECISION_DATE' })
   decisionDate: Date;
-  
-  @Column({ name: "DECISION_TYPE_ID" })
+
+  @Column({ name: 'DECISION_TYPE_ID' })
   departmentTypeId: number;
 
   @OneToOne(() => PriAdministrativeDecType, (dt) => dt.decisionTypeId)
-  @JoinColumn({name: 'DECISION_TYPE_ID'})
+  @JoinColumn({ name: 'DECISION_TYPE_ID' })
   decisionType: PriAdministrativeDecType;
 
-  @Column({ name: "EMPLOYEE_ID" })
+  @Column({ name: 'EMPLOYEE_ID' })
   employeeId: number;
 
   @OneToOne(() => PriEmployee, (pe) => pe.employeeId)
-  @JoinColumn({name: 'EMPLOYEE_ID'})
+  @JoinColumn({ name: 'EMPLOYEE_ID' })
   employee: PriEmployee;
 
-  @Column({ name: "EMPLOYEE_NAME" })
+  @Column({ name: 'EMPLOYEE_NAME' })
   employeeName: string;
 
-  @Column({ name: "DEPARTMENT_ID" })
+  @Column({ name: 'DEPARTMENT_ID' })
   departmentId: number;
 
   @OneToOne(() => PriInfoDepartment, (d) => d.departmentId)
-  @JoinColumn({name: 'DEPARTMENT_ID'})
+  @JoinColumn({ name: 'DEPARTMENT_ID' })
   department: PriInfoDepartment;
-  
-  @Column({ name: "CREATED_DATE", default: new Date() })
+
+  @Column({ name: 'CREATED_DATE', default: new Date() })
   createdDate: Date;
 
-  @Column({ name: "CONTENT" })
+  @Column({ name: 'CONTENT' })
   content: string;
 
-  @Column({ name: "CREATED_EMPLOYEE_KEY_ID" })
+  @Column({ name: 'CREATED_EMPLOYEE_KEY_ID' })
   createdEmployeeKeyId: number;
 
   constructor(item: Partial<PriAdministrativeDecision>) {
-    Object.assign(this, item)
+    Object.assign(this, item);
   }
 
   /*
-    * Table-ын талбаруудын мэдээлэл
-  */
+   * Table-ын талбаруудын мэдээлэл
+   */
   static getTableFields() {
     return {
-      administrativeDecisionId: { header: 'ID', type: 'number', sortable: true, filterable: false, width: 'w-16' },
-      administrativeDecisionNumber: { header: 'Шийдвэрийн дугаар', type: 'string', sortable: true, filterable: true, width: 'w-48' },
-      decisionDate: { header: 'Шийдвэрийн огноо', type: 'date', sortable: true, filterable: true, width: 'w-48' },
-      'decisionType.name': { header: 'Шийдвэрийн төрөл', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      'department.name': { header: 'Алба хэлтэс', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      'employee.person.firstName': { header: 'Албан хаагч', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      createdDate: { header: 'Үүсгэсэн огноо', type: 'date', sortable: true, filterable: false, width: 'w-16' },
+      administrativeDecisionId: {
+        header: 'ID',
+        type: 'number',
+        sortable: true,
+        filterable: false,
+        width: 'w-16',
+      },
+      administrativeDecisionNumber: {
+        header: 'Шийдвэрийн дугаар',
+        type: 'string',
+        sortable: true,
+        filterable: true,
+        width: 'w-48',
+      },
+      decisionDate: {
+        header: 'Шийдвэрийн огноо',
+        type: 'date',
+        sortable: true,
+        filterable: true,
+        width: 'w-48',
+      },
+      'decisionType.name': {
+        header: 'Шийдвэрийн төрөл',
+        type: 'ref',
+        refField: 'decisionType.name',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      'department.name': {
+        header: 'Алба хэлтэс',
+        type: 'ref',
+        refField: 'department.name',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      'employee.person.firstName': {
+        header: 'Албан хаагч',
+        type: 'ref',
+        refField: 'employee.person.firstName',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      createdDate: {
+        header: 'Үүсгэсэн огноо',
+        type: 'date',
+        sortable: true,
+        filterable: false,
+        width: 'w-16',
+      },
     };
   }
 }
--- main/src/entity/pri/employee/priEmployeeKey.ts
+++ main/src/entity/pri/employee/priEmployeeKey.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
 import { PriEmployee } from './priEmployee';
 import { PriInfoPositionType } from 'src/entity/info/priInfoPositionType';
 import { PriInfoMilitaryRank } from 'src/entity/info/priInfoMilitaryRank';
@@ -6,60 +12,99 @@ import { PriInfoDepartment } from 'src/entity/info/priInfoDepartment';
 
 @Entity('PRI_EMPLOYEE_KEY')
 export class PriEmployeeKey {
-
-  @PrimaryGeneratedColumn({ name: "EMPLOYEE_KEY_ID" })
+  @PrimaryGeneratedColumn({ name: 'EMPLOYEE_KEY_ID' })
   employeeKeyId: number;
 
-  @Column({ name: "EMPLOYEE_ID" })
+  @Column({ name: 'EMPLOYEE_ID' })
   employeeId: number;
 
   @OneToOne(() => PriEmployee, (pe) => pe.employeeId)
-  @JoinColumn({name: 'EMPLOYEE_ID'})
+  @JoinColumn({ name: 'EMPLOYEE_ID' })
   employee: PriEmployee;
 
-  @Column({ name: "POSITION_TYPE_ID" })
+  @Column({ name: 'POSITION_TYPE_ID' })
   positionTypeId: number;
 
   @OneToOne(() => PriInfoPositionType, (pt) => pt.positionTypeId)
-  @JoinColumn({name: 'POSITION_TYPE_ID'})
+  @JoinColumn({ name: 'POSITION_TYPE_ID' })
   positionType: PriInfoPositionType;
 
-  @Column({ name: "MILITARY_RANK_ID" })
+  @Column({ name: 'MILITARY_RANK_ID' })
   militaryRankId: number;
 
   @OneToOne(() => PriInfoMilitaryRank, (mr) => mr.militaryRankId)
-  @JoinColumn({name: 'MILITARY_RANK_ID'})
+  @JoinColumn({ name: 'MILITARY_RANK_ID' })
   militaryRank: PriInfoMilitaryRank;
 
-  @Column({ name: "DEPARTMENT_ID" })
+  @Column({ name: 'DEPARTMENT_ID' })
   departmentId: number;
 
   @OneToOne(() => PriInfoDepartment, (d) => d.departmentId)
-  @JoinColumn({name: 'DEPARTMENT_ID'})
+  @JoinColumn({ name: 'DEPARTMENT_ID' })
   department: PriInfoDepartment;
 
-  @Column({ name: "IS_ACTIVE" })
+  @Column({ name: 'IS_ACTIVE' })
   isActive: boolean;
 
-  @Column({ name: "CREATED_DATE", default: new Date() })
+  @Column({ name: 'CREATED_DATE', default: new Date() })
   createdDate: Date;
 
   constructor(item: Partial<PriEmployeeKey>) {
-    Object.assign(this, item)
+    Object.assign(this, item);
   }
 
   /*
-    * Table-ын талбаруудын мэдээлэл
-  */
+   * Table-ын талбаруудын мэдээлэл
+   */
   static getTableFields() {
     return {
       // employeeKeyId: { header: 'ID', type: 'number', sortable: true, filterable: false, width: 'w-16' },
-      employeeId: { header: 'ID', type: 'number', sortable: true, filterable: false, width: 'w-16' },
-      'employee.person.stateRegNumber': { header: 'Регистр', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      'employee.person.lastName': { header: 'Эцэг/Эх-ийн нэр', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      'employee.person.firstName': { header: 'Өөрийн нэр', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      'employee.user.userName': { header: 'Хэрэглэгчийн нэр', type: 'ref', sortable: false, filterable: true, width: 'w-16' },
-      createdDate: { header: 'Бүртгэсэн Огноо', type: 'date', sortable: true, filterable: false, width: 'w-40' },
+      employeeId: {
+        header: 'ID',
+        type: 'number',
+        sortable: true,
+        filterable: false,
+        width: 'w-16',
+      },
+      'employee.person.stateRegNumber': {
+        header: 'Регистр',
+        type: 'ref',
+        refField: 'employee.person.stateRegNumber',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      'employee.person.lastName': {
+        header: 'Эцэг/Эх-ийн нэр',
+        type: 'ref',
+        refField: 'employee.person.lastName',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      'employee.person.firstName': {
+        header: 'Өөрийн нэр',
+        type: 'ref',
+        refField: 'employee.person.firstName',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      'employee.user.userName': {
+        header: 'Хэрэглэгчийн нэр',
+        type: 'ref',
+        refField: 'employee.user.userName',
+        sortable: false,
+        filterable: true,
+        width: 'w-16',
+      },
+      createdDate: {
+        header: 'Бүртгэсэн Огноо',
+        type: 'date',
+        sortable: true,
+        filterable: false,
+        width: 'w-40',
+      },
     };
   }
 }
